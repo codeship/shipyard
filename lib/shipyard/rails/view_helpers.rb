6e0dffe29e0e60c2286c765592b744a57b7bbacb
Ryan Wilke <ryanwilke@gmail.com>
2017-04-25 17:17:17 +0200

    Worked on heroes and buttons.

--- lib/shipyard/rails/view_helpers.rb
+++ lib/shipyard/rails/view_helpers.rb
@@ -1,6 +1,15 @@
 module Shipyard
   module Rails
     module ViewHelpers
+      def hero(options={}, &block)
+        @hero_type = options[:class].try(:include?, 'playful') ? :playful : :default
+        options[:class] = "hero #{options[:class]}".strip
+        options[:data] = { hero: '' }
+        content_tag :div, options do
+          content_tag :div, capture(&block), class: 'hero-container rjw-container'
+        end
+      end
+
       def btn(name, *args, &block)
         if block_given?
           args << name
