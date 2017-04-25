d14f63ff6a66efcd78fe93664f428ecef2fadb05
Ryan Wilke <ryanwilke@gmail.com>
2017-04-25 18:51:44 +0200

    Got the layout helpers included.

---
+++ lib/shipyard/rails/layout_helpers.rb
@@ -0,0 +1,45 @@
+module Shipyard
+  module Rails
+    module LayoutHelpers
+      def layout_classes(layout_name)
+        "#{layout_name}-layout #{current_page} #{'legacy-content' unless content_for? :new_layout}"
+      end
+
+      def header_classes
+        case @hero_type
+        when :default
+          'header header-with-hero'
+        when :playful
+          'header header-with-hero header-inverse'
+        else
+          'header'
+        end
+      end
+
+      def current_page
+        "#{controller.controller_name.dasherize}-#{controller.action_name}"
+      end
+
+      def current_route
+        "#{controller.controller_name}##{controller.action_name}"
+      end
+
+      def current_route_is?(routes)
+        routes.tr(' ', '').split(',').include? current_route
+      end
+
+      def content(options={}, &block)
+        type, options = options, {} if options.is_a? Symbol
+        options[:class] ||= ''
+        options[:class] +=  type ? " content-#{type.to_s.dasherize}" : ' content'
+        options[:class] +=  " #{current_page}-content"
+        options[:data] ||= {}
+        options[:data][:content] = current_page
+        content_tag :div, options do
+          concat render(partial: '/layouts/alerts')
+          concat capture(&block)
+        end
+      end
+    end
+  end
+end
--- lib/shipyard/rails/railtie.rb
+++ lib/shipyard/rails/railtie.rb
@@ -1,10 +1,12 @@
 require 'shipyard/rails/view_helpers'
+require 'shipyard/rails/layout_helpers'
 
 module Shipyard
   module Rails
     class Railtie < ::Rails::Railtie
       initializer 'shipyard.view_helpers' do
         ActionView::Base.send :include, ViewHelpers
+        ActionView::Base.send :include, LayoutHelpers
       end
     end
   end
