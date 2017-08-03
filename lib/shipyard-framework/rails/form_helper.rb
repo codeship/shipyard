46602db226eada17ec79a94222114750cb862e37
Ryan Wilke <ryanwilke@gmail.com>
2017-08-03 18:11:57 +0200

    Added the input_text helper and bumped to 0.2.9

--- lib/shipyard-framework/rails/form_helper.rb
+++ lib/shipyard-framework/rails/form_helper.rb
@@ -1,6 +1,11 @@
 module Shipyard
   module Rails
     module FormHelper
+      def input_text(name, value=nil, options={})
+        options[:class] = "input input-text #{options[:class]}"
+        text_field_tag name, value, options
+      end
+
       def input_select_tag(name, choices, container_options={}, select_options={})
         container_options[:class] = "input-select-container #{container_options[:class]}"
         content_tag :div, container_options do
--- lib/shipyard-framework/version.rb
+++ lib/shipyard-framework/version.rb
@@ -1,3 +1,3 @@
 module Shipyard
-  VERSION = '0.2.8'
+  VERSION = '0.2.9'
 end
