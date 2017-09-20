e215b882ae7f42db422e2d35107b7150ed57c9dc
Ryan Wilke <ryanwilke@gmail.com>
2017-09-20 17:25:53 +0200

    Added test for custom classes.

--- spec/helpers/form_helper_spec.rb
+++ spec/helpers/form_helper_spec.rb
@@ -9,9 +9,21 @@ RSpec.describe Shipyard::FormHelper, type: :helper do
     )
   end
 
+  it 'should return a text box with a custom CSS class' do
+    expect(input_text(:organization, 'Shipyard', class: 'shipyard-input')).to(
+      match(%r{<input type="text" name="organization" id="organization" value="Shipyard" class="input input-text shipyard-input" />})
+    )
+  end
+
   it 'should return a select box' do
     expect(input_select_tag(:organization, %w(HTML CSS JavaScript))).to(
       match(%r{<div class="input-select-container"><select name="organization" id="organization" class="input input-select"><option value="HTML">HTML</option>\n<option value="CSS">CSS</option>\n<option value="JavaScript">JavaScript</option></select></div>})
+    )
+  end
+
+  it 'should return a select box wth a custom CSS class' do
+    expect(input_select_tag(:organization, %w(HTML CSS JavaScript), class: 'shipyard-select')).to(
+      match(%r{<div class="input-select-container shipyard-select"><select name="organization" id="organization" class="input input-select"><option value="HTML">HTML</option>\n<option value="CSS">CSS</option>\n<option value="JavaScript">JavaScript</option></select></div>})
     )
   end
 end
