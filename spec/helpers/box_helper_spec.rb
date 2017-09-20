1e498652ee530c77310dc12558711745f8719b06
Ryan Wilke <ryanwilke@gmail.com>
2017-09-20 15:31:46 +0200

    Change size to type.

--- spec/helpers/box_helper_spec.rb
+++ spec/helpers/box_helper_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe Shipyard::BoxHelper, type: :helper do
   end
 
   it 'should return all box types' do
-    %w(secondary padding link).each do |size|
-      expect(box([size.to_sym], 'test')).to match(%r{<div class="box box-#{size}">test</div>})
+    %w(secondary padding link).each do |type|
+      expect(box([type.to_sym], 'test')).to match(%r{<div class="box box-#{type}">test</div>})
     end
   end
 
