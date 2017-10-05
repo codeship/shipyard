a11cc48f0ebc188244d220f68fe33928881f8a91
Ryan Wilke <ryanwilke@gmail.com>
2017-10-05 13:41:50 +0100

    Update the Railtie to pass in the rails directories.

--- lib/shipyard-framework/rails/railtie.rb
+++ lib/shipyard-framework/rails/railtie.rb
@@ -2,10 +2,12 @@ module Shipyard
   module Rails
     class Railtie < ::Rails::Railtie
       initializer 'shipyard.reload_cached_icons' do
-        if ::Rails.env.development?
-          ::Rails.application.reloaders << Shipyard::Icons.instance
-          ::Rails.application.config.to_prepare { Shipyard::Icons.instance.execute_if_updated }
-        end
+        # TODO: Use rake task or hook into asset pipeline instead?
+        # rake shipyard:icons["#{::Rails.root}/app/assets/icons/", "#{::Rails.root}/public/assets"]
+        ::Rails.application.reloaders << Shipyard::Icons.new(
+          "#{::Rails.root}/app/assets/icons/",
+          "#{::Rails.root}/public/assets"
+        )
       end
     end
   end
