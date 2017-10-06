6c73c6324b5f81e83d53d1f0b321e47491c18948
Ryan Wilke <ryanwilke@gmail.com>
2017-10-06 12:12:41 +0100

    Fixed the Rails generation issues.

--- lib/shipyard-framework/rails/railtie.rb
+++ lib/shipyard-framework/rails/railtie.rb
@@ -4,10 +4,11 @@ module Shipyard
       initializer 'shipyard.reload_cached_icons' do
         # TODO: Use rake task or hook into asset pipeline instead?
         # rake shipyard:icons["#{::Rails.root}/app/assets/icons/", "#{::Rails.root}/public/assets"]
-        ::Rails.application.reloaders << Shipyard::Icons.new(
+        $icons = Shipyard::Icons.new(
           "#{::Rails.root}/app/assets/icons/",
           "#{::Rails.root}/public/assets"
         )
+        ::Rails.application.reloaders << $icons
       end
     end
   end
