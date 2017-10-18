56e43c4fbbfc11aabab5043d4ace623ef62d0c6a
Ryan Wilke <ryan@codeship.com>
2017-10-18 18:01:02 +0200

    Removed my email.

--- ci/build.rb
+++ ci/build.rb
@@ -3,4 +3,3 @@ require_relative '../lib/shipyard-framework/version'
 puts "\n\n************** Build & Push to Rubygems.org **************"
 system("bash -c 'gem build shipyard.gemspec'")
 system("bash -c 'gem push shipyard-framework-#{Shipyard::VERSION}.gem'")
-system("bash -c 'echo ryanwilke@gmail.com'")
