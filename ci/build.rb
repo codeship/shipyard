f82eb796b3a5a18e3aab19529d295101809919ba
Ryan Wilke <ryan@codeship.com>
2017-10-18 16:12:54 +0200

    Remove the verbose option.

--- ci/build.rb
+++ ci/build.rb
@@ -2,4 +2,4 @@ require_relative '../lib/shipyard-framework/version'
 
 puts "\n\n************** Build & Push to Rubygems.org **************"
 system("bash -c 'gem build shipyard.gemspec'")
-system("bash -c 'gem push -V shipyard-framework-#{Shipyard::VERSION}.gem'")
+system("bash -c 'gem push shipyard-framework-#{Shipyard::VERSION}.gem'")
