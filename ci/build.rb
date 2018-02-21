e4c9e2bd7fa2a529246060c5c9e9404bf62e5b4d
Ryan Wilke <ryanwilke@gmail.com>
2018-02-21 10:24:00 +0100

    Fixed the deploy steps

--- ci/build.rb
+++
@@ -1,5 +0,0 @@
-require_relative '../lib/shipyard-framework/version'
-
-puts "\n\n************** Build & Push to Rubygems.org **************"
-system("bash -c 'gem build shipyard.gemspec'")
-system("bash -c 'gem push shipyard-framework-#{Shipyard::VERSION}.gem'")
