a85cfa905f9ae74af3bdd3e7afc23491259d0d48
Ryan Wilke <ryanwilke@gmail.com>
2018-03-14 10:24:02 +0100

    Improved the media css output.

--- styleguide/_plugins/sass_output.rb
+++ styleguide/_plugins/sass_output.rb
@@ -11,12 +11,12 @@ module Jekyll
       output = sass['compressed_css']
       output.gsub! /{/, ' { '
       output.gsub! /}([^}])/, " }\n\\1" # Match closing brackets
-      output.gsub! /@media(.*)\) { /, "@media\\1) {\n" # Match @media opening brackets
+      output.gsub! /@media(.*)\) { /, "\n@media\\1) {\n" # Match @media opening brackets
       output.gsub! /} }/, " }\n}" # Match @media closing brackets
       output.gsub! /([a-z0-9]*),\./, "\\1,\n." # Match each declaration
       output.gsub! /\),./, "),\n." # Match :not(...),.class
       output.gsub! /([a-z]):(?!:|not|hover)/, '\1: \2' # Match CSS properties
-      output.gsub! /\\:/, ':' # Clean up the responsive classes
+      output.gsub! /([a-z.]*)\\:/, '  \1:' # Clean up the responsive classes
       output.gsub! /;/, '; '
       output.gsub! /\n\z/, ''
       output
