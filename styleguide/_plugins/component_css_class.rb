6768f22e698e207bbe32369fb49cdd2dfbd785f7
Ryan Wilke <ryanwilke@gmail.com>
2018-03-15 15:12:40 +0100

    Worked on some small issues I noticed in Percy.

--- styleguide/_plugins/component_css_class.rb
+++ styleguide/_plugins/component_css_class.rb
@@ -1,11 +1,12 @@
 module Jekyll
   module ComponentCssClassFilter
     def component_css_class(element, breakpoint, modifier=nil)
+      has_breakpoint = ['sm', 'md', 'lg', 'xl'].include? breakpoint
+      modifier ||= breakpoint unless has_breakpoint
       css = []
-      css << "#{breakpoint}:" if breakpoint && !modifier.nil?
+      css << "#{breakpoint}:" if has_breakpoint
       css << element
       css << "-#{modifier}" if modifier
-      css << "-#{breakpoint}" if modifier.nil?
       css.join
     end
   end
