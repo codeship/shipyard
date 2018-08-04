6b23f44ff322d790706e3cc71256e2b48b954022
Josh south <jsouth@users.noreply.github.com>
2018-08-04 15:40:59 +0200

    Prod-Perfect Tracking (#130)
    * Prod-Perfect Tracking
    
    * Upgrade some rubygems

--- styleguide/_assets/js/vendor/prod-perfect.js
+++ styleguide/_assets/js/vendor/prod-perfect.js
@@ -1,12 +1,12 @@
 !function(name,path,ctx){
   var latest,prev=name!=='Keen'&&window.Keen?window.Keen:false;ctx[name]=ctx[name]||{ready:function(fn){var h=document.getElementsByTagName('head')[0],s=document.createElement('script'),w=window,loaded;s.onload=s.onreadystatechange=function(){if((s.readyState&&!(/^c|loade/.test(s.readyState)))||loaded){return}s.onload=s.onreadystatechange=null;loaded=1;latest=w.Keen;if(prev){w.Keen=prev}else{try{delete w.Keen}catch(e){w.Keen=void 0}}ctx[name]=latest;ctx[name].ready(fn)};s.async=1;s.src=path;h.parentNode.insertBefore(s,h)}}
-}('ProdPerfectKeen','https://d1vnucvopbiinv.cloudfront.net/keen-tracking.min.js',this);
+}('ProdPerfectKeen','https://codeship.trackinglibrary.prodperfect.com/keen-tracking.min.js',this);
 ProdPerfectKeen.ready(function(){
   var client = new ProdPerfectKeen({
     projectId: '5adce3eac9e77c0001b895bd',
     writeKey: '0AD5C295946F8E8FE30167600BABBF7109AD0EEE4DAA1970771BA8FC73637C9758B7F4ED5E1AA64034C5B3B7E0C3FE7B9A273A455C11C203CC419238512402821EFB41FA87190840A3D68F271C282C5D50D4808BC0E68BC3252B2ADAF433285D',
     requestType: 'beacon',
-    host: 'ti6jgi88s4.execute-api.us-west-2.amazonaws.com/Production'
+    host: 'codeship.datapipe.prodperfect.com/v1'
   });
   client.extendEvents({
     visitor: {
